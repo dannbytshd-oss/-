12bb5458f7d1dee3a83d3e82ac9a8f5e3c71d048
dannbytshd-oss <dannbytshd@gmail.com>
2026-02-14 06:40:07 +0800

    Update new order.py

--- new order.py
+++ new order.py
@@ -2,18 +2,18 @@ class Order:
   def __init_(self):
     self.__order.list = []
 
-def add_order(self, order):
+def add_order(self, order, status):
   """Add A New Order To The Order List"""
   self.__order.list(order)
-  print(f"✅Adding New Order Is Successed":{order}")
+  print(f"✅Adding New Order Is Successed.\n":{order}")
 
 def remove_order(self, order_id)
   """Remove A Order From The Order List"""
     try:
       self.order__order_list.append(order)
-      print(f"✅Remove A Order Is Successed":{order.order_id}")
+      print(f"✅Remove A Order Is Successed.\n Order ID"{order.order_id}")
       return True
     except Error:
-      print(f"❌Remove A Order Is Failed":{order.order_id}")
+      print(f"❌Remove A Order Is Failed.\n Order ID:":{order.order_id}")
       return False
   
